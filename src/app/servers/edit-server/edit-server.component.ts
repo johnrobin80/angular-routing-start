5daefc6cc672b15989e212d9ae38d66d8f04928c
robin-john-menaa <robinprojectgcp@gmail.com>
2021-10-19 18:07:38 +0400

    Routing Settings

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { ServersService } from '../servers.service';
+import { ServersService } from "../servers.service";
 
 @Component({
-  selector: 'app-edit-server',
-  templateUrl: './edit-server.component.html',
-  styleUrls: ['./edit-server.component.css']
+  selector: "app-edit-server",
+  templateUrl: "./edit-server.component.html",
+  styleUrls: ["./edit-server.component.css"],
 })
 export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
-  serverName = '';
-  serverStatus = '';
+  server: { id: number; name: string; status: string };
+  serverName = "";
+  serverStatus = "";
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService) {}
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
@@ -21,7 +21,9 @@ export class EditServerComponent implements OnInit {
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus,
+    });
   }
-
 }
