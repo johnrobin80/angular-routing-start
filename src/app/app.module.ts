99cb9db290a1627caad276946b5c7ea51fdcd891
robin-john-menaa <robinprojectgcp@gmail.com>
2021-10-31 18:49:05 +0400

    Routing from separate module.
    Routing from separate module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { Routes, RouterModule } from "@angular/router";
+// import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
@@ -11,30 +11,34 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AppRoutingModule } from "./app-routing.module";
 
-const appRoutes: Routes = [
-  { path: "", component: HomeComponent },
-  // { path: "users", component: UsersComponent },
-  // { path: "users/:id/:name", component: UserComponent },
-  // { path: "servers", component: ServersComponent },
-  // { path: "servers/:id", component: ServerComponent },
-  // { path: "servers/:id/edit", component: EditServerComponent },
+// const appRoutes: Routes = [
+//   { path: "", component: HomeComponent },
+//   // { path: "users", component: UsersComponent },
+//   // { path: "users/:id/:name", component: UserComponent },
+//   // { path: "servers", component: ServersComponent },
+//   // { path: "servers/:id", component: ServerComponent },
+//   // { path: "servers/:id/edit", component: EditServerComponent },
 
-  {
-    path: "users",
-    component: UsersComponent,
-    children: [{ path: ":id/:name", component: UserComponent }],
-  },
+//   {
+//     path: "users",
+//     component: UsersComponent,
+//     children: [{ path: ":id/:name", component: UserComponent }],
+//   },
 
-  {
-    path: "servers",
-    component: ServersComponent,
-    children: [
-      { path: ":id", component: ServerComponent },
-      { path: ":id/edit", component: EditServerComponent },
-    ],
-  },
-];
+//   {
+//     path: "servers",
+//     component: ServersComponent,
+//     children: [
+//       { path: ":id", component: ServerComponent },
+//       { path: ":id/edit", component: EditServerComponent },
+//     ],
+//   },
+//   { path: "not-found", component: PageNotFoundComponent },
+//   { path: "**", redirectTo: "/not-found" },
+// ];
 
 @NgModule({
   declarations: [
@@ -45,8 +49,10 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  // imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
