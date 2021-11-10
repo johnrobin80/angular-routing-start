897c92a7197a7b8b4f567af798663991dde961df
robin-john-menaa <robinprojectgcp@gmail.com>
2021-11-10 16:04:06 +0400

    Updates

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -3,15 +3,32 @@ import {
   CanActivate,
   RouterStateSnapshot,
   ActivatedRouteSnapshot,
+  Router,
+  CanActivateChild,
 } from "@angular/router";
 import { Observable } from "rxjs/observable";
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: "root",
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean> | Promise<boolean> | boolean {}
+  ): Observable<boolean | any> | Promise<boolean | any> | boolean | any {
+    this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        return true;
+      } else {
+        this.router.navigate(["/"]);
+      }
+    });
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | any> | Promise<boolean | any> | boolean | any {}
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,4 +5,23 @@ import { Injectable } from "@angular/core";
 })
 export class AuthService {
   constructor() {}
+  loggedIn = false;
+
+  isAuthenticated() {
+    // tslint:disable-next-line:no-shadowed-variable
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 800);
+    });
+    return promise;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
 }
